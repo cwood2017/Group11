17bfd96e11ae3025ed42f7942a2a81b3d3146538
Levi Terry <ldterry14@gmail.com>
2019-02-05 15:15:40 -0700

    Made changes to my function in group.c

--- group.c
+++ group.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-void leviT()
-{
-// Test
+void leviT(){
+  printf("My name is Levi.");
+  printf("I am technically a Senior with over 140+ credits, but for all intensive purposes I am a sophomore");
+  printf("Major is Computer Science");
+  printf("I like to play/listen to music, fish, climb, hike, shoot, play video games, and read.");
+  printf("I was born and raised in Tennessee.");
+  printf("I was in the Navy for 9 years.");
 }
 
 void codyV()
